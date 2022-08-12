421b7d8116e04eb9170b1f52122e923e1a470e85
KalyanRam <kalyan.2405@gmail.com>
2022-08-12 15:26:55 +0530

    For deploy

--- office_emp_project/office_emp_project/settings.py
+++ office_emp_project/office_emp_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'py+vel06x=w^n$r0sgt&hk6^r-49io4=(ncl4384qwmptgtjge'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
